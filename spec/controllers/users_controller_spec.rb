6c1c40230b67f6ff2d5bd8a73fefefb4ff497766
Quy <quy.nguyenngoctp@gmail.com>
2017-08-15 18:06:59 +0700

    do EXTRA BONUS of milestone 2

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,11 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+
+  let(:valid_attributes) { {name: "Quy", email: "quy123@gmail.com", password: "1234"} } 
+  let(:valid_user) {User.create(valid_attributes)}
+  let(:valid_session) { {user_id: valid_user.id} }
   context "user is logged in" 
     describe "GET#index" do 
       it "returns a success respone" do 
         user = User.create name: "Quy", email: "quy123@gmail.com", password: "1234"
-        get :index, params: {}, session: {user_id: user.id}
+        get :index, params: {}, session: valid_session
+        expect(response).to be_success   
+      end 
+    end 
+
+  context "user is logged in - version update let ()" 
+    describe "GET#index" do 
+      it "returns a success respone" do 
+        get :index, params: {}, session: valid_session
         expect(response).to be_success   
       end 
     end 
