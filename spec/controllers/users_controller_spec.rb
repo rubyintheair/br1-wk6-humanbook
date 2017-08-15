de9208aab092da9d223adb158de52192dc2f2d55
Quy <quy.nguyenngoctp@gmail.com>
2017-08-15 18:23:02 +0700

    edit user_controller_spec style

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe UsersController, type: :controller do
   let(:valid_attributes) { {name: "Quy", email: "quy123@gmail.com", password: "1234"} } 
   let(:valid_user) {User.create(valid_attributes)}
   let(:valid_session) { {user_id: valid_user.id} }
-  context "user is logged in" 
+  context "user is logged in" do 
     describe "GET#index" do 
       it "returns a success respone" do 
         user = User.create name: "Quy", email: "quy123@gmail.com", password: "1234"
@@ -14,23 +14,15 @@ RSpec.describe UsersController, type: :controller do
       end 
     end 
 
-  context "user is logged in - version update let ()" 
-    describe "GET#index" do 
+  
+    describe "GET#index version update valid_session" do 
       it "returns a success respone" do 
         get :index, params: {}, session: valid_session
         expect(response).to be_success   
       end 
     end 
 
-  context "user is not logged in" 
-    describe "GET#index" do 
-      it "redirects to the home page" do 
-        get :index, params: {}, session: {}
-        expect(response).to redirect_to(root_path)
-      end 
-    end 
-
-  context "user is logged"
+  
     describe "Get#index" do 
       it "loads all users" do 
         user = User.create name: "Adam", email: "123@gmail.com", password: "1234"
@@ -38,8 +30,17 @@ RSpec.describe UsersController, type: :controller do
         expect(assigns(:users)).to eq [user]
       end 
     end 
+  end 
 
-  
+  context "user is not logged in" do
+    describe "GET#index" do 
+      it "redirects to the home page" do 
+        get :index, params: {}, session: {}
+        expect(response).to redirect_to(root_path)
+      end 
+    end 
+  end 
+
 
 
 end
